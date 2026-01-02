7a9a36d343b0b68c65891728f9a4d85006b2983a
Satriahrl_ <satriaherlambang22@gmail.com>
2026-01-02 19:55:52 +0700

    integrasi firebase cloud messaging

---
+++ lib/pages/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // menyiapkan variable yang akan di isi melalui data notification
+  String _message = "No message received yet, waiting...";
+
+  @override
+  void initState() {
+    super.initState();
+    setupFCM();
+  }
+
+  Future<void> setupFCM() async {
+    print("Setting up Firebase Cloud Messaging...");
+    final messaging = FirebaseMessaging.instance;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            const Text(
+              'Welcome to selamat datang!',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            Text(_message, style: const TextStyle(fontSize: 20)),
+          ],
+        ),
+      ),
+    );
+  }
+}
